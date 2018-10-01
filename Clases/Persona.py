002e999f8ef8ef7fe0b97b2cff66e9bb8e39b8f0
Angatupyry <fierrofenix@gmail.com>
2018-10-01 13:40:00 -0400

    Cambios en Persona

---
+++ Clases/Persona.py
@@ -0,0 +1,11 @@
+from abc import ABCMeta
+
+class Persona(metaclass=ABCMeta):
+
+    def __init__(self, cedula, nombre, apellido, direccion = None, contacto = None):
+        self.cedula = cedula
+        self.nombre = nombre
+        self.apellido = apellido
+        self.direccion = direccion
+        self.contactos = []
+        self.contactos.append(contacto)
